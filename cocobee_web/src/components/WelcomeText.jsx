104dbcaf91e2162ac8b0579c87a23020bdeddb9f
IshanVimukthi <75219861+Ishan-Vimukthi@users.noreply.github.com>
2025-02-22 09:21:44 +0530

    Welcome Text component

---
+++ cocobee_web/src/components/WelcomeText.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const WelcomeText = () => {
+  return (
+    <div className='p-10 flex flex-col items-center justify-center'>
+      <h3>WELCOME TO MR.COCOBEE</h3>
+      <p>TO INSPIRE TO OFFER A HEALTHY FOOD AND LIFESTYLE CHOICE FOR EVERYONE</p>
+    </div>
+  )
+}
+
+export default WelcomeText
--- cocobee_web/src/pages/Home.jsx
+++ cocobee_web/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
+import WelcomeText from '../components/WelcomeText'
 
 const Home = () => {
   return (
     <div>
       <NavBar/>
+      <WelcomeText/>
     </div>
   )
 }
